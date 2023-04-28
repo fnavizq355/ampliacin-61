fcd2259ae6cc8b2d9898c323d00c36106c24d1cf
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-04-28 09:10:15 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,57 @@
+input.onButtonPressed(Button.A, function () {
+    if (cesta != 0 && t <= fin) {
+        led.unplot(cesta, 4)
+        cesta = cesta - 1
+        led.plot(cesta, 4)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (cesta != 4 && t <= fin) {
+        led.unplot(cesta, 4)
+        cesta = cesta + 1
+        led.plot(cesta, 4)
+    }
+})
+let y = 0
+let x = 0
+let cesta = 0
+let t = 0
+let fin = 0
+fin = 20
+let p = 0
+t = 0
+let v = 500
+cesta = 2
+led.plot(cesta, 4)
+basic.forever(function () {
+    while (t <= fin) {
+        x = randint(1, 4)
+        y = 0
+        led.plot(x, y)
+        for (let index = 0; index < 4; index++) {
+            if (t <= fin) {
+                basic.pause(v)
+                led.unplot(x, y)
+                y = y + 1
+                led.plot(x, y)
+            }
+        }
+        if (x == cesta && y == 4) {
+            p = p + 1
+            v = v - 25
+        } else {
+            if (t <= fin) {
+                led.unplot(x, y)
+            }
+        }
+        basic.pause(v)
+    }
+})
+basic.forever(function () {
+    while (t <= fin) {
+        basic.pause(1000)
+        t = t + 1
+    }
+    basic.clearScreen()
+    basic.showString("" + (p))
+})
