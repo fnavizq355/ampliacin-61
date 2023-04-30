a0988d4eb10170ef2d07a5a04f85636a31aeec3c
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-04-30 12:24:51 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,23 @@ input.onButtonPressed(Button.A, function () {
         led.plot(cesta, 4)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    p = 0
+    v = 500
+    cesta = 2
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    led.plot(cesta, 4)
+    fin = 0
+    t = 0
+    while (t <= 5) {
+        basic.pause(1000)
+        t = t + 1
+    }
+    fin = 1
+})
 input.onButtonPressed(Button.B, function () {
     if (cesta < 4 && fin == 0) {
         led.unplot(cesta, 4)
@@ -12,20 +29,14 @@ input.onButtonPressed(Button.B, function () {
         led.plot(cesta, 4)
     }
 })
-let t = 0
 let y = 0
 let x = 0
+let t = 0
+let v = 0
+let p = 0
 let cesta = 0
 let fin = 0
-fin = 0
-let p = 0
-let v = 500
-cesta = 2
-basic.showNumber(3)
-basic.showNumber(2)
-basic.showNumber(1)
-basic.clearScreen()
-led.plot(cesta, 4)
+fin = 1
 basic.forever(function () {
     if (fin == 0) {
         x = randint(0, 4)
@@ -48,15 +59,9 @@ basic.forever(function () {
             led.unplot(x, y)
         } else {
             basic.showNumber(p)
+            basic.pause(500)
+            basic.clearScreen()
         }
         basic.pause(v)
     }
-})
-basic.forever(function () {
-    t = 0
-    while (t <= 20) {
-        basic.pause(1000)
-        t = t + 1
-    }
-    fin = 1
 })
